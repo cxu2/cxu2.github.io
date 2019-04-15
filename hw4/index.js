ccdcbf79a8dfeb23000e3804201b8402bab8dec7
cx2168@columbia.edu <cxu2>
2019-04-15 03:08:46 +0000

    colab -> github

--- hw4/index.js
+++ hw4/index.js
@@ -12,7 +12,9 @@ const examples = {
   'example2':
       'Buda-Pesth seems a wonderful place.',
   'example3':
-      'Scepticism was as much the result of knowledge, as knowledge is of scepticism.'      
+      'Scepticism was as much the result of knowledge, as knowledge is of scepticism.' 
+  'example4':
+      'So I kissed his hand, and lay quiet, while he proceeded to indite a note to Biddy, with my love in it.'
 };
 
 function status(statusText) {
